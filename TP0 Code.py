0aa37fd35bec4b527ce031e5740afc3bda1f842f
cathySY <74866864+cathySY@users.noreply.github.com>
2020-11-22 23:09:54 +0800

    Sunday night
    Setting folder up to use cmu graphics

--- TP0 Code.py
+++ TP0 Code.py
@@ -1,3 +1,25 @@
-hello hello!
-testing if this backup works for github
-trying again
+#################################################
+# TP0 Code.py
+#
+# Your name: Yi Sijun Cathy
+# Your andrew id: sijuncay
+#################################################
+
+import cs112_f20_week7_linter
+import math, copy, random
+
+from cmu_112_graphics import *
+
+from cmu_112_graphics import *
+
+def appStarted(app):
+    app.counter = 0
+
+def keyPressed(app, event):
+    app.counter += 1
+
+def redrawAll(app, canvas):
+    canvas.create_text(app.width/2, app.height/2,
+                       text=f'{app.counter} keypresses', font='Arial 30 bold')
+
+runApp(width=400, height=400)
